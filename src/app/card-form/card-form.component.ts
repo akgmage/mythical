615f35b0ce71ba846a2650006c8a615718fdec67
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-14 23:18:06 +0530

    Add validators to Cardnumber, expiration and security code

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -8,9 +8,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)],),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl(''),
+    cardNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
+    // Start of string should be 0 followed by 1-9 or 1 followed by 0, 1 or 2
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
   });
   constructor() { }
 
