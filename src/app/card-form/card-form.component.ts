b3457d1e5a5d655f89cdbad4d86f23a17e34942e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-14 22:50:36 +0530

    Add additional form controls

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -7,15 +7,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl(
-      '', 
-      [
-        Validators.required, 
-        Validators.minLength(3),
-        // Validators.maxLength(6),
-        // Validators.pattern(/\s/)
-      ],
-    ),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)],),
+    cardNumber: new FormControl(''),
+    expiration: new FormControl(''),
+    securityCode: new FormControl(''),
   });
   constructor() { }
 
