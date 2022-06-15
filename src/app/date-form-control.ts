53177c2063cf86ed7f10f3a5895a565655560f1c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-15 23:40:19 +0530

    Handle null values

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -1,9 +1,13 @@
 import { FormControl } from "@angular/forms";
 
 export class DateFormControl extends FormControl {
-    override setValue(value: string, options: any) {
+    override setValue(value: string | null, options: any) {
+        if(!value) {
+            super.setValue(value, {...options, emitModelToViewChange: true});
+            return;
+        }
         if (value.match(/[^0-9|\/]/gi)) {
-            super.setValue(this.value, {...options, emitModelToViewChange: true});
+            super.setValue('', {...options, emitModelToViewChange: true});
             return;
         }
         if( value.length > 5) {
