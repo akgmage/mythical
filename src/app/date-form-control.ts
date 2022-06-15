427acacec96cb46b5af5915e931c4443610bf6e6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-15 23:05:48 +0530

    Disallow letters in expiration date

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -2,6 +2,11 @@ import { FormControl } from "@angular/forms";
 
 export class DateFormControl extends FormControl {
     override setValue(value: string, options: any) {
+        if (value.match(/[^0-9|\/]/gi)) {
+            super.setValue(this.value, {...options, emitModelToViewChange: true});
+            return;
+        }
         console.log(value);
+        super.setValue(value, options);
     }
 }
