9f6f8eff54ebbb01f2c2cf2e2764d0d180a85782
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-15 22:48:27 +0530

    Crete an instance of DateFormControl and use that to define expiration field

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
@@ -10,7 +11,7 @@ export class CardFormComponent implements OnInit {
     name: new FormControl('', [Validators.required, Validators.minLength(3)],),
     cardNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
     // Start of string should be 0 followed by 1-9 or 1 followed by 0, 1 or 2
-    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    expiration: new DateFormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
     securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
   });
   constructor() { }
